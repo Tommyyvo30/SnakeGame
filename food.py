3023791009bfabf96441d2bfecbbe9fb17b52f80
Tommyyvo30 <tommyyvo.tv@gmail.com>
2023-02-18 12:38:39 -0800

    added highscore

--- food.py
+++ food.py
@@ -1,13 +1,14 @@
 from turtle import *
 import random
 
+
 class Food(Turtle):
 
     def __init__(self):
         super().__init__()
         self.shape("circle")
         self.penup()
-        self.shapesize(stretch_len= 0.5, stretch_wid=0.5)
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("red")
         self.speed("fastest")
         self.refresh()
--- main.py
+++ main.py
@@ -25,16 +25,22 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    # Collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
-    if snake.head.xcor() > 295 or snake.head.xcor() < -295 or snake.head.ycor() > 295 or snake.head.ycor() < - 295:
-        game_is_on = False
-        scoreboard.game_over()
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
 
+    # Collision with wall
+    if snake.head.xcor() > 295 or snake.head.xcor() < -295 or snake.head.ycor() > 295 or snake.head.ycor() < - 295:
+        scoreboard.reset()
+        snake.reset()
+    # Collision with tail
+    for segment in snake.segments[1:]:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
 screen.exitonclick()
